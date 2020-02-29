9b98f3cdc09bfd75d53c04ce9016f11fc1c37079
paul <drawmindmap@gmail.com>
2020-02-29 15:13:08 +0800

    Add favicon.ico

--- tools/rollup.config.js
+++ tools/rollup.config.js
@@ -10,6 +10,7 @@ const banner = `<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="shortcut icon" href="data:;base64,iVBORw0KGgo=">
   <style>
     html,
     body {
